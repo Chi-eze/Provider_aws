7f325cc1c75a6a4d521fc626b28111d41c18735b
chi-eze <chieze@mail.com>
2019-04-02 14:53:53 +0100

    Testing input-var

--- main.tf
+++ main.tf
@@ -8,8 +8,34 @@ provider "aws" {
 resource "aws_instance" "supermax" {
     ami = "ami-07683a44e80cd32c5"
     instance_type =  "t2.micro"    
+    key_name = "ceze"
+    
 
     tags = {
-        Name = "Terraform_test"
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+        Name = "Web-Serve"
+    }         
+
+
+}
+
+variable "server_port" {
+    Description = "The port used for HTTP request"
+    default = 22
+}
+
+resource "aws_security_group" "web-access" {
+    name = "web-access"
+    ingress {
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+            tags {
+        Name = "Test-Chi"
+    
+    }
+
+   
 }
