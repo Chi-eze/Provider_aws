01bfff6662e0b6ff54852fc7e7868597185f97a8
chi-eze <chieze@mail.com>
2019-04-02 16:11:17 +0100

    adding security group id to to VPC_security_group_ids parameter

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "supermax" {
     ami = "ami-07683a44e80cd32c5"
     instance_type =  "t2.micro"    
     key_name = "ceze"
+    vpc_security_group_ids = ["${aws_security_group.web-access.id}"]
     
 
     tags = {
@@ -19,7 +20,7 @@ resource "aws_instance" "supermax" {
 }
 
 variable "server_port" {
-    Description = "The port used for HTTP request"
+    description = "The port used for HTTP requests"
     default = 22
 }
 
@@ -27,7 +28,7 @@ resource "aws_security_group" "web-access" {
     name = "web-access"
     ingress {
         from_port = "${var.server_port}"
-        to_port = "${var.server_port}"
+        to_port = "${var.server_port}" 
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
