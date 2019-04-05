5f47b8850b1565b2e709772ff1793585b14e44ed
chi-eze <chieze@mail.com>
2019-04-05 20:09:47 +0100

    autoscaling test

--- main.tf
+++ main.tf
@@ -1,33 +1,68 @@
+
+#----------------------variables-----------------------
+
+
+#output "public_ip" {
+#  value = "${aws_instance.supermax.public_ip}"
+#}
+
+output "elb_dns_name" {
+  value = "${aws_elb.elb-ai.dns_name}"
+}
+data "aws_availability_zones" "all" {}
+
+
+
 # ----------------Configure aws Provider---------------------------------#
 
 provider "aws" {
     region = "eu-west-1"
 }
 
+
 # -----------------Building an EC2 instance----------------
 resource "aws_instance" "supermax" {
     ami = "ami-07683a44e80cd32c5"
     instance_type = "t2.micro"    
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "hello,world" > index.html
+                nohup busybox httpd -f -p "${var.webaccess}" &
+                EOF
     key_name = "${var.key_name}"
     vpc_security_group_ids = ["${aws_security_group.web-access.id}"]
     tags = {
         Name = "Web-Serve"
     }         
 
-
 }
+#--------------------launch_Configuration---------------------
 
-variable "server_port" {
-    description = "The port used for HTTP request"
-    default = 22
+resource "aws_launch_configuration" "lc_ai" {
+    name = "cluster_temp"
+    image_id = "ami-07683a44e80cd32c5"
+    instance_type = "m4.4xlarge"
+    key_name = "${var.key_name}"
+    security_groups = ["${aws_security_group.web-access.id}"]
+    user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install httpd -y
+                cd /var/www/html
+                touch index.html
+                echo > "Hello, World" /var/www/html/index.html
+                chkconfig httpd on
+                EOF
+
+                lifecycle {
+                    create_before_destroy = true 
+                }
   
 }
 
-output "public_ip" {
-  value = "${aws_instance.supermax.public_ip}"
-}
+  
 
-
+#--------------------------security..............................
 
 resource "aws_security_group" "web-access" {
     name = "web-connect"
@@ -35,15 +70,88 @@ resource "aws_security_group" "web-access" {
         from_port = "${var.server_port}"
         to_port = "${var.server_port}" 
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [ "${var.cidr_blocks}"]
+    }
+    ingress {
+        from_port = "${var.webaccess}"
+        to_port = "${var.webaccess}"
+        protocol = "tcp"
+        cidr_blocks = [ "${var.cidr_blocks}"]
     }
 
             tags {
         Name = "Test-exec"
     
     }
-   
+
+    lifecycle {
+        create_before_destroy = true 
+    }   
   }
+resource "aws_security_group" "elb_sg" {
+
+    name = "elb_security"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["${var.cidr_blocks}"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks =["${var.cidr_blocks}"]
+    }
+  
+}
 
 
 
+
+
+
+   
+
+resource "aws_autoscaling_group" "asg_ai" {
+    min_size = 2
+    max_size = 6   
+    launch_configuration = "${aws_launch_configuration.lc_ai.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    load_balancers = ["${aws_elb.elb-ai.id}"]
+    health_check_type = "ELB"
+    tag {
+        Key = "explicit1"
+        value = "terraform-asg-example05"
+        propagate_at_launch = true
+
+    }
+
+  
+}
+
+
+#-------------------------ELB-------------------------------
+
+resource "aws_elb" "elb-ai" {
+    name = "elb"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb_sg.id}"]
+
+    listener {
+        lb_port = 80
+        lb_protocol ="http"
+        instance_port = "${var.webaccess}"
+        instance_protocol = "http"
+    }
+    health_check  {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target ="http:${var.webaccess}/var/www/html/index.html"
+    }
+
+  
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
 variable "key_name" {}
+variable "cidr_blocks" {
+    type 
+    = "list"
+    default=[]
+}
+variable "server_port" {
+    description = "The port used for ssh access"
+    default = ""
+ 
+}
+
+variable "webaccess" {
+    description = "The port used for HTTP request"
+    default = ""
+  
+}
+
+
+
